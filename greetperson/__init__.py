a40aaccb0570586b96e43fbc5689f5ef73bbee2d
HassnaIrzan <hassna.irzan.17@ucl.ac.uk>
2023-08-24 11:56:06 +0100

    delete greetperson dir and change setup.py

--- greetperson/__init__.py
+++
@@ -1,1 +0,0 @@
-from .print_hello import GreetPerson
--- setup.py
+++ setup.py
@@ -8,13 +8,14 @@ with open(path.join(current_directory, 'README.md'), encoding='utf-8') as f:
 
 
 setup(
-    name="greetperson",
-    version="0.1",
+    name="greet",
+    version="0.0.1",
     author="Hassna",
     author_email="rmaphir@gmail.com",
     license="MIT",
     description="A package to greet a person by name.",
     long_description=long_description,
-    packages=['greetperson'],
+    packages=['greet'],
+    package_dir={'greet': 'src'},
     zip_safe=False,
 )
